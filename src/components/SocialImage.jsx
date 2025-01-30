fc12fe62f43b4dfd01d62d0937bdbfb4fa1d0d7a
Duane Morin <duane@fortinbras.local>
2025-01-30 09:55:50 -0500

    share buttons and extended social data

---
+++ src/components/SocialImage.jsx
@@ -0,0 +1,13 @@
+const SocialImage = () => {
+  return (
+    <div className="flex mt-8 justify-center pb-8">
+      <img
+        src="/assets/images/pointing-shakespeare.png"
+        alt="Shakespeare Geek"
+        className="w-1/4 max-w-xs rounded"
+      />
+    </div>
+  );
+};
+
+export default SocialImage;
